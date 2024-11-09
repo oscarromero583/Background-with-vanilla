86b84cb9d29bf792dde0bd28ae79d125f108c474
oscarromero583 <obetaoscar583@gmail.com>
2024-11-09 06:54:33 +0100

    new feature - css and js files 	modified:   index.html 	modified:   main.js 	modified:   style.css

---
+++ main.js
@@ -0,0 +1,15 @@
+const canvas = document.getElementById('effect');
+const ctx = canvas.getContext('2d');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const particles = [];
+
+function initParticles() {
+    for (let i = 0; i < 200; i++) {
+        particles.push ({
+            x: canvas.width /
+        })
+    }
+}
